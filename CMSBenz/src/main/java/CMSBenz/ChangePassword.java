75c5293da5d89196ac4300806c48e720a44e3f6f
BS2896 <BS2896@ISU1SW002-PC.isolvegroup.org>
2018-06-25 14:30:14 +0530

    First Commit-25062018

---
+++ CMSBenz/src/main/java/CMSBenz/ChangePassword.java
@@ -0,0 +1,57 @@
+package CMSBenz;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChangePassword {
+		WebDriver driver;
+	
+	public void loadBrowser() throws Exception{  // Loading Browser
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\SeleniumDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://172.16.0.74/isolvecms");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("arun"); 
+		System.out.println("Entered UserName");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("CMS123$");
+		System.out.println("Entered Password");
+		driver.findElement(By.xpath("//button[@id='btnSubmit']")).click();
+		System.out.println("Clicked Login Button");
+		Thread.sleep(3000);
+	}
+	
+	public void chnagePwdCancel() throws InterruptedException {
+		System.out.println("Cancel Change Password");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html/body/nav[2]/ul/li[3]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"oldPassword\"]")).sendKeys("CMS123$");
+		driver.findElement(By.xpath("//*[@id=\"newPassword\"]")).sendKeys("iSolve123$");
+		driver.findElement(By.xpath("//*[@id=\"ConfirmPassword\"]")).sendKeys("iSolve123$");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"divForgotPasswordwindow\"]/div/center/div/div[2]/form/div[4]/div[2]/button")).click();
+	}
+	
+	public void chnagePwd() throws InterruptedException {
+		System.out.println("Change Password");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html/body/nav[2]/ul/li[3]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"oldPassword\"]")).sendKeys("CMS123$");
+		driver.findElement(By.xpath("//*[@id=\"newPassword\"]")).sendKeys("iSolve123$");
+		driver.findElement(By.xpath("//*[@id=\"ConfirmPassword\"]")).sendKeys("iSolve123$");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"btnChangePassword\"]")).click();
+		
+	}
+	public static void main(String[] args) throws Exception {
+		ChangePassword change = new ChangePassword();
+		change.loadBrowser();
+		change.chnagePwdCancel();
+		change.loadBrowser();
+		change.chnagePwd();
+	}
+}
---
+++ CMSBenz/src/main/java/CMSBenzRead/ReadExcel.java
@@ -0,0 +1,66 @@
+package CMSBenzRead;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+	
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	static Row row;
+	static int rowNos;
+	static Cell cell;
+	
+	public static void loadExcel() throws Exception{
+		
+//		System.out.println("Loading Excel :::");
+		FileInputStream File = new FileInputStream("D:\\Senthil\\Java Program\\CMSBenz\\src\\test\\java\\CMSBenz\\Data.xlsx");
+		workbook = new XSSFWorkbook(File);
+		sheet = workbook.getSheet("Sheet1");
+		File.close();
+		
+//		rowNos = sheet.getPhysicalNumberOfRows();
+//		System.out.println(rowNos);
+		}
+	
+	public static Map<String, Map<String, String>> getDatamap() throws Exception {
+		if(sheet==null) {
+			loadExcel();
+		}
+		Map<String, Map<String,String>> superMap = new HashMap<String, Map<String,String>>();
+		Map<String,String> secondMap = new HashMap<String,String>();
+		
+		for(int i=1 ;i<sheet.getLastRowNum()+1;i++) {
+			row = sheet.getRow(i);			
+			String keyCell = row.getCell(0).getStringCellValue();
+		for(int j=1;j<row.getLastCellNum();j++) {
+				
+			cell=row.getCell(j);				
+			cell.setCellType(Cell.CELL_TYPE_STRING);
+			
+			String Value = row.getCell(j).getStringCellValue();
+			secondMap.put(keyCell, Value);
+		} 
+		 superMap.put("MASTERDATA",secondMap );
+	}
+		return superMap;	
+	}
+	
+	public static String getValue(String key) throws Exception {
+		Map<String, String> myVal = getDatamap().get("MASTERDATA");
+		String retValue = myVal.get(key);
+		return retValue;
+		}
+	
+//	public static void main(String[] args) throws Exception {
+//		System.out.println(getValue("login_username"));
+//		System.out.println(getValue("login_pwd"));
+//	}
+	}
